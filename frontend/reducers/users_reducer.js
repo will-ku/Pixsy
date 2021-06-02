7b9c959c63d35aca754de719336e05a7c0cb3bdb
will-ku <william.ku9192@gmail.com>
2021-06-01 20:15:52 -0400

    Additional user auth updates

--- frontend/reducers/users_reducer.js
+++ frontend/reducers/users_reducer.js
@@ -1,16 +1,12 @@
-import { RECEIVE_CURRENT_USER } from '../actions/session_actions';
-
-import { RECEIVE_REVIEW, RECEIVE_BENCH } from '../actions/bench_actions';
+import { RECEIVE_CURRENT_USER } from "../actions/session_actions";
 
 const usersReducer = (state = {}, action) => {
   Object.freeze(state);
-  switch(action.type) {
+  switch (action.type) {
     case RECEIVE_CURRENT_USER:
-      return Object.assign({}, state, { [action.currentUser.id]: action.currentUser });
-    case RECEIVE_REVIEW:
-      return Object.assign({}, state, { [action.author.id]: action.author });
-    case RECEIVE_BENCH:
-      return Object.assign({}, state, action.authors);
+      return Object.assign({}, state, {
+        [action.currentUser.id]: action.currentUser,
+      });
     default:
       return state;
   }
