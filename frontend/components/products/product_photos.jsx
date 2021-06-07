5f0c0e311b6c59b6fe23cd09f302414b5a5abb8c
will-ku <william.ku9192@gmail.com>
2021-06-07 14:59:46 -0400

    product photos

---
+++ frontend/components/products/product_photos.jsx
@@ -0,0 +1,59 @@
+import React from "react";
+
+class ProductPhotos extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currPhotoIdx: 0,
+    };
+    this.changePhoto = this.changePhoto.bind(this);
+    // this.clickPhoto = this.clickPhoto.bind(this);
+  }
+
+  changePhoto(change) {
+    const { product } = this.props;
+    const newPhotoIdx =
+      (change + this.state.currPhotoIdx + product.photoUrl.length) %
+      product.photoUrl.length;
+    this.setState({ currPhotoIdx: newPhotoIdx });
+  }
+
+  // clickPhoto(idx) {
+  //   this.setState({ currPhotoIdx: idx });
+  // }
+
+  render() {
+    const { product } = this.props;
+    return (
+      <div className="product-show-photos-flex-box">
+        <div className="product-show-carousel">
+          <ul>
+            {product.photoUrl.map((photo, idx) => {
+              return (
+                <li key={idx}>
+                  <img
+                    className="product-show-carousel-photo"
+                    // onClick={() => this.clickPhoto(idx)}
+                    src={product.photoUrl[idx]}
+                  />
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+
+        <div className="product-show-main-photo">
+          <button className="next-photo" onClick={() => this.changePhoto(-1)}>
+            previous
+          </button>
+          <img src={product.photoUrl[this.state.currPhotoIdx]} />
+          <button className="prev-photo" onClick={() => this.changePhoto(1)}>
+            next
+          </button>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default ProductPhotos;
