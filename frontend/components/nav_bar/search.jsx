fef29778b244052a3e273e0fc84f1598417741b4
will-ku <william.ku9192@gmail.com>
2021-06-29 17:21:20 -0400

    updated object-fit on photos

--- frontend/components/nav_bar/search.jsx
+++ frontend/components/nav_bar/search.jsx
@@ -1,22 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import { MdSearch } from "react-icons/md";
 
-class Search extends React.Component {
-  render() {
-    return (
-      <form className="search-component">
-        <input
-          className="search-bar"
-          type="text"
-          size="30"
-          placeholder="Search bar under construction"
-        />
-        <button className="search-button" type="submit">
-          <MdSearch size={24} />
-        </button>
-      </form>
-    );
-  }
+function Search() {
+  const [term, setTerm] = useState();
+
+  return (
+    <form className="search-component">
+      <input
+        className="search-bar"
+        type="text"
+        size="30"
+        placeholder="Search bar under construction"
+      />
+      <button className="search-button" type="submit">
+        <MdSearch size={24} />
+      </button>
+    </form>
+  );
 }
 
 export default Search;
--- frontend/reducers/session_errors_reducer.js
+++ frontend/reducers/session_errors_reducer.js
@@ -20,4 +20,3 @@ export default (state = [], action) => {
       return state;
   }
 };
-]
